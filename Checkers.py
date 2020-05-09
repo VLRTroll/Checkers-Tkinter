e3cc6b204d777b9754831e3fb542bf216d7ea9e6
VLRTroll <55628273+VLRTroll@users.noreply.github.com>
2020-05-09 15:12:38 -0300

    Piece position system explained and Constants added

--- Checkers.py
+++ Checkers.py
@@ -2,13 +2,22 @@ from tkinter import *
 
 # Constants
 BOARD_SIZE = 400
-DRAUGHTS_COUNT = 8
-DRAUGHT_SIZE = BOARD_SIZE/DRAUGHTS_COUNT
+ROWS_FILLED = 3
+
+DRAUGHTS_IN_ROW = 8
+DRAUGHT_SIZE = BOARD_SIZE/DRAUGHTS_IN_ROW
+PIECE_SIZE = int(0.8 * DRAUGHT_SIZE)
+
+DARK_DRAUGHT_COLOR = "black"
+LIGHT_DRAUGHT_COLOR = "white"
+
+DARK_PIECE_COLOR = "red"
+LIGHT_PIECE_COLOR = "#cfaf50"
 
 # Settings
 game = Tk()
 game.title("Checkers")
-game.geometry('400x400+100+100')
+game.geometry(f'{BOARD_SIZE}x{BOARD_SIZE}+100+100')
 game.resizable(0,0)
 
 board = Frame(game)
@@ -18,35 +27,63 @@ draughts = Canvas(board, width = BOARD_SIZE, height = BOARD_SIZE)
 draughts.pack(fill = 'both')
 
 # Draw an 8×8 draughts board
+DRAUGHT_COLOR = DARK_DRAUGHT_COLOR
 
-DRAUGHT_COLOR = 'black'
-
-for row in range(DRAUGHTS_COUNT + 1):
-    for column in range(DRAUGHTS_COUNT + 1):
-        DRAUGHT_COLOR = 'white' if DRAUGHT_COLOR == 'black' else 'black'
+for row in range(DRAUGHTS_IN_ROW + 1):
+    for column in range(DRAUGHTS_IN_ROW + 1):
+        DRAUGHT_COLOR = LIGHT_DRAUGHT_COLOR if DRAUGHT_COLOR == DARK_DRAUGHT_COLOR else DARK_DRAUGHT_COLOR
         
         draught = draughts.create_rectangle(row*DRAUGHT_SIZE,column*DRAUGHT_SIZE,\
                                             (row*DRAUGHT_SIZE)+DRAUGHT_SIZE,(column*DRAUGHT_SIZE)+DRAUGHT_SIZE,\
                                             fill = DRAUGHT_COLOR, outline = DRAUGHT_COLOR)
 
-# Impressão das Peças Vermelhas
-for i in range(0,4):
-    #Dimensão da imagem e distância entre a borda da imagem e o inicio da mesma
-    row1 = draughts.create_oval(40+100*i,40,10+100*i,10,fill="red")
-    borda1 = draughts.create_oval(35+100*i,35,15+100*i,15)
-    row2 = draughts.create_oval(90+100*i,90,60+100*i,60,outline=None,fill="red")
-    borda1 = draughts.create_oval(85+100*i,85,65+100*i,65)
-    row3 = draughts.create_oval(40+100*i,140,10+100*i,110,outline=None,fill="red")
-    borda1 = draughts.create_oval(35+100*i,135,15+100*i,115)
 
-# Impressão das Peças Marrons
-for i in range(0,4):
-    #Dimensão da imagem e distância entre a borda da imagem e o inicio da mesma
-    row1 = draughts.create_oval(90+100*i,390,60+100*i,360,fill="#cfaf50")
-    borda1 = draughts.create_oval(85+100*i,385,65+100*i,365)
-    row2 = draughts.create_oval(40+100*i,340,10+100*i,310,fill="#cfaf50")
-    borda2 = draughts.create_oval(35+100*i,335,15+100*i,315)
-    row3 = draughts.create_oval(90+100*i,290,60+100*i,260,fill="#cfaf50")
-    borda3 = draughts.create_oval(85+100*i,285,65+100*i,265)
+# Draw the pieces
+'''
+Piece coordinates on the canvas
+O----------------------------------O
+| CANVAS                           |
+|                                  |
+|   (x1,y1) x-----------O          |
+|           |           |          |
+|           |           |          |
+|           |   PIECE   |          |
+|           |           |          |
+|           |           |          |
+|           O-----------x (x0,y0)  |
+|                                  |
+O----------------------------------O
+'''
+
+draw_piece = lambda x0,y0,x1,y1,color:\
+             draughts.create_oval(x0, y0, x1, y1, fill = color)
+
+draw_dark_piece = lambda x0,y0,x1,y1:\
+                  draw_piece(x0, y0, x1, y1, DARK_PIECE_COLOR)
+
+draw_light_piece = lambda x0,y0,x1,y1:\
+                   draw_piece(x0, y0, x1, y1, LIGHT_PIECE_COLOR)
+
+
+for i in range(ROWS_FILLED + 1):
+    ## --- Draw the dark pieces --- ##
+    row1 = draw_dark_piece(40+100*i,40,10+100*i,10)
+    borda1 = draw_dark_piece(35+100*i,35,15+100*i,15)
+    
+    row2 = draw_dark_piece(90+100*i,90,60+100*i,60)
+    borda1 = draw_dark_piece(85+100*i,85,65+100*i,65)
+    
+    row3 = draw_dark_piece(40+100*i,140,10+100*i,110)
+    borda1 = draw_dark_piece(35+100*i,135,15+100*i,115)
+
+    ## --- Draw the light pieces --- ##
+    row1 = draw_light_piece(90+100*i,390,60+100*i,360)
+    borda1 = draw_light_piece(85+100*i,385,65+100*i,365)
+    
+    row2 = draw_light_piece(40+100*i,340,10+100*i,310)
+    borda2 = draw_light_piece(35+100*i,335,15+100*i,315)
+    
+    row3 = draw_light_piece(90+100*i,290,60+100*i,260)
+    borda3 = draw_light_piece(85+100*i,285,65+100*i,265)
 
 game.mainloop()
